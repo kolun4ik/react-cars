58acbf70eef72684de82b5b19455df4c1451b3c3
kolun4ik <11_ka@rambler.ru>
2020-01-02 18:54:54 +0300

    Работа со списком

--- src/App.js
+++ src/App.js
@@ -17,37 +17,40 @@ class App extends React.Component {
         });
     }
 
+    handleInput = (event) => {
+        this.setState({
+            pageTitle: event.target.value,
+        })
+    }
+
   render() {
       const divStyle = {
           textAlign: 'center'
       }
-    const cars = this.state.cars
     return (
         <div style={divStyle}>
             <h1>{this.state.pageTitle}</h1>
+
+            <input  type="text" onChange={this.handleInput}/>
+
             {/*bind -первый параметр тот контекст, с которым должна быть вызвана ф-ция,
             в нашем случае это this. bind будет возвращать новую ф-цию, но не вызывать ee.
             Далее, через запятую, любой набор параметров (первый вариант передачи параметров)*/}
-
             <button
                 onClick={this.changeTitleHandler.bind(this, ' Chenged')}>
                 Change title</button>
-
-            <Car
-                name={cars[0].name}
-                year={cars[0].year}
-                onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)} //Событие можно назвать как хочется
-            />
-            {/*Второй вариант передачи параметров: через стрелочную ф-цию*/}
-            <Car
-                name={cars[1].name}
-                year={cars[1].year}
-                onChangeTitle={() => this.changeTitleHandler(cars[1].name)}
-            />
-            <Car
-                name={cars[2].name}
-                year={cars[2].year}
-            />
+            {/*Выводим список всех машин, где car - каждый из объектов списка*/}
+            { this.state.cars.map((car, index) => {
+                return (
+                    //каждый объект преобразуем в компонент
+                    <Car
+                        key={index}
+                        name={car.name}
+                        year={car.year}
+                        onChangeTitle={() => this.changeTitleHandler(car.name)}
+                    />
+                )
+            }) }
         </div>
     )
   }
