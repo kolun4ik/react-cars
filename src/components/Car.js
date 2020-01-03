b9fd0168d9938381f8ad0e7882aeb2e866433b69
kolun4ik <11_ka@rambler.ru>
2020-01-03 03:11:50 +0300

    Добавили использование hoc (Higher-Order Component)

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,46 +1,11 @@
 import React from "react";
-// import Radium from 'radium';
-import './Car.css';
+import classes from './Car.css';
+import withClass from '../hoc/withClass';
 
 class Car extends React.Component {
 
-    componentWillReceiveProps(nextProps, nextContext) {
-        console.log('Car componentWillReceiveProps', nextProps);
-    }
-
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        console.log('Car shouldComponentUpdate', nextProps, nextState);
-        return nextProps.name.trim() !== this.props.name.trim();
-    }
-
-    componentWillUpdate(nextProps, nextState, nextContext) {
-        console.log('Car componentWillUpdate', nextProps, nextState);
-    }
-
-    // static getDerivedStateFromProps(nextProps, prevState) {
-    //     console.log('Car getDerivedStateFromProps', nextProps, prevState);
-    //     return prevState;
-    // }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('Car componentDidUpdate');
-    }
-
-    // getSnapshotBeforeUpdate(prevProps, prevState) {
-    //     console.log('Car getSnapshotBeforeUpdate');
-    // }
-
-    componentWillUnmount() {
-        console.log('Car componentWillUnmount');
-    }
 
     render() {
-        console.log('Car Render');
-
-        // if(Math.random() > 0.7){
-        //     throw new Error('Car random failed')
-        // }
-
         const inputClasses = ['input'];
 
         if(this.props.name !== ''){
@@ -54,18 +19,9 @@ class Car extends React.Component {
             inputClasses.push('bold');
         }
 
-        const style =  {
-            border: '1px solid #ccc',
-            boxShadow: '0 4px 5px 0 rgba(0,0,0, 0.14)',
-            ':hover': {
-                border: '1px solid #aaa',
-                boxShadow: '0 4px 15px 0 rgba(0,0,0, 0.25)',
-                cursor: 'pointer'
-            }
-        }
 
         return (
-            <div className="Car" style={style}>
+            <React.Fragment>
                 <h3>Car name: {this.props.name}</h3>
                 <p>YEAR: {this.props.year}</p>
                 <input
@@ -75,9 +31,9 @@ class Car extends React.Component {
                     className={inputClasses.join(' ')} // массив преобразуем в строку
                 />
                 <button onClick={this.props.onDelete}>Delete</button>{/*обращаемся как референс, к той ф-ции, которую передаем*/}
-            </div>
+            </React.Fragment>
         );
     }
 }
 
-export default Car;
+export default withClass(Car, classes.Car);
